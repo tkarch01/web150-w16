e64ab6e897eb0a8eddee6ff19cc31c6c00314e47
tkarchesy <tkarch01@seattlecentral.edu>
2016-01-16 05:49:58 +0000

    homework assignment 2

---
+++ assignment-2/script.js
@@ -0,0 +1,6 @@
+// load up the fortunes into an array
+var fortunes =["You will get an 'A' on a test.", "You will be rich.", "You will have very good luck today.", "You will have many friends.", "Do a good deed today.", "Someone will call you today.", "You will go to a party soon.", "Be careful on Tuesday"];
+
+// load up the colors into an array
+var colors = ["Red", "Blue", "Green", "Yellow"];
+
